313d1ebe69b450aca56a4a0d683b9db462071b04
Ian Denhardt <ian@zenhack.net>
2023-04-18 01:54:21 -0400

    Wrap a bit of the stream API

--- streams/reader.go
+++ streams/reader.go
@@ -1,18 +1,46 @@
 package streams
 
 import (
+	"io"
+	"syscall/js"
+
 	"zenhack.net/go/jsapi"
+	"zenhack.net/go/jsapi/promise"
 )
 
 type ReadableStreamDefaultReader jsapi.Value
 
-type ReadResult struct {
-	Value jsapi.Value // TODO: type for the chunk.
-	Done  bool
+func (r ReadableStreamDefaultReader) Next() ([]byte, error) {
+	v, err := promise.Promise[jsapi.Value]{Value: r.Value.Call("read")}.Wait()
+	if err != nil {
+		return nil, err
+	}
+	if v.Value.Get("done").Bool() {
+		return nil, io.EOF
+	}
+	chunk := v.Value.Get("theChunk")
+	length := chunk.Call("length").Int()
+	result := make([]byte, length)
+	copied := js.CopyBytesToGo(result, chunk)
+	if copied != length {
+		panic("short copy")
+	}
+	return result, nil
 }
 
-/*
-func (r ReadableStreamDefaultReader) Read() promise.Promise[ReadResult] {
-	panic("TODO")
+func (r ReadableStreamDefaultReader) WriteTo(w io.Writer) (n int64, err error) {
+	for {
+		chunk, err := r.Next()
+		if err == io.EOF {
+			return n, nil
+		}
+		if err != nil {
+			return n, err
+		}
+		m, err := w.Write(chunk)
+		n += int64(m)
+		if err != nil {
+			return n, err
+		}
+	}
 }
-*/
